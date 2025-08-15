8287ed5e5a601c72c11226e555bb59c2210b71fd
runtingt <t.runting23@imperial.ac.uk>
2025-08-15 18:16:33 +0100

    Ruff

--- tests/test_formatting.py
+++ tests/test_formatting.py
@@ -11,7 +11,7 @@ class TestFormatTable:
         mock_schedd = mocker.Mock()
         mock_schedd.query.return_value = TEST_JOBS
         user_stats = fetch_jobs(None, mock_schedd)
-        
+
         current_user = "test_user0"
         tab = format_table(
             user_stats=user_stats,
@@ -20,7 +20,7 @@ class TestFormatTable:
             priority=priority,
             user_priorities={"test_user0": 1.0, "test_user1": 2.0},
         )
-        
+
         assert tab is not None
         assert len(tab._rows) == len(user_stats) + 1
         if priority:
@@ -30,7 +30,7 @@ class TestFormatTable:
                 assert "GPU" not in tab.field_names
             elif only.lower() == "gpu":
                 assert "CPU" not in tab.field_names
-        
+
     def test_format_table_empty(self):
         with pytest.raises(SystemExit) as excinfo:
             format_table(user_stats={}, current_user="test_user0", only=None, priority=False, user_priorities={})
--- tests/test_htcondor.py
+++ tests/test_htcondor.py
@@ -9,7 +9,7 @@ TEST_JOBS = [
         "ClusterId": 12345,
         "JobStatus": 1,
         "RemoteHost": "cpu1.example.com",
-        "RequestGPUs": 0
+        "RequestGPUs": 0,
     },
     {
         "Owner": "test_user0",
@@ -17,29 +17,17 @@ TEST_JOBS = [
         "ClusterId": 12345,
         "JobStatus": 2,
         "RemoteHost": "cpu2.example.com",
-        "RequestGPUs": 0
+        "RequestGPUs": 0,
     },
-    {
-        "Owner": "test_user0",
-        "ProcId": 1,
-        "ClusterId": 12345,
-        "JobStatus": 5,
-        "RequestGPUs": 0
-    },
-    {
-        "Owner": "test_user0",
-        "ProcId": 1,
-        "ClusterId": 12345,
-        "JobStatus": 5,
-        "RequestGPUs": 1
-    },
+    {"Owner": "test_user0", "ProcId": 1, "ClusterId": 12345, "JobStatus": 5, "RequestGPUs": 0},
+    {"Owner": "test_user0", "ProcId": 1, "ClusterId": 12345, "JobStatus": 5, "RequestGPUs": 1},
     {
         "Owner": "test_user1",
         "ProcId": 0,
         "ClusterId": 12346,
         "JobStatus": 1,
         "RemoteHost": "gpu1.example.com",
-        "RequestGPUs": 1
+        "RequestGPUs": 1,
     },
     {
         "Owner": "test_user1",
@@ -47,7 +35,7 @@ TEST_JOBS = [
         "ClusterId": 12346,
         "JobStatus": 2,
         "RemoteHost": "gpu2.example.com",
-        "RequestGPUs": 1
+        "RequestGPUs": 1,
     },
 ]
 
@@ -55,27 +43,24 @@ EXPECTED_RESULT = {
     "test_user0": {
         "CPU": {"Idle": 1, "Running": 1, "Held": 1},
         "GPU": {"Held": 1},
-        "Total": {"Idle": 1, "Running": 1, "Held": 2}
+        "Total": {"Idle": 1, "Running": 1, "Held": 2},
     },
-    "test_user1": {
-        "CPU": {},
-        "GPU": {"Idle": 1, "Running": 1},
-        "Total": {"Idle": 1, "Running": 1}
-    }
+    "test_user1": {"CPU": {}, "GPU": {"Idle": 1, "Running": 1}, "Total": {"Idle": 1, "Running": 1}},
 }
+
 
 class TestFetchJobs:
     @pytest.mark.parametrize("only", [None, "cpu"])
     def test_fetch_jobs(self, mocker, only):
         mock_schedd = mocker.Mock()
         mock_schedd.query.return_value = TEST_JOBS
-        
+
         result = fetch_jobs(only, mock_schedd)
         result = {user: {k: dict(v) for k, v in stats.items()} for user, stats in result.items()}
         if only:
             expected_result = {
                 user: {k: v for k, v in stats.items() if k != "GPU"}
-                for user, stats in EXPECTED_RESULT.items() 
+                for user, stats in EXPECTED_RESULT.items()
                 if stats[only.upper()] != {}
             }
             for results in expected_result.values():
