bbb5af2338e91638e1288b7214cbc330799a3a28
Tom Runting <thomas.runting@cern.ch>
2026-04-30 16:45:59 +0100

    Move away from vols

--- condor_stat.py
+++ condor_stat.py
@@ -1,4 +1,4 @@
-#! /vols/cms/tr1123/condor_tools/env/bin/python
+#! /home/hep/tr1123/.local/share/condor_tools/env/bin/python
 """Entry point script for condor_stat"""
 
 from condor_tools.condor_tools import main
